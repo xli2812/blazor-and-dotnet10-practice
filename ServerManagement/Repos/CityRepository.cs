d5a2aa6710a1ed87fd955a314cea4fba348766d3
Xavier <elementler@hotmail.com>
2026-02-24 11:41:33 -0800

    Render buttons for cities

---
+++ ServerManagement/Repos/CityRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Repos
+{
+    public static class CityRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+    }
+}
